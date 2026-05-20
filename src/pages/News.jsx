57ae8bd7349620dea30d5b88c4ccd565751aa86a
Mike Tsakiroglou <32255755+MikeTsak@users.noreply.github.com>
2026-05-20 16:02:38 +0300

    feat(News): add Open TV to news outlets list

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -22,6 +22,7 @@ const NEWS_OUTLETS = {
   'MEGA': { name: 'Mega Gegonota', logo: 'https://upload.wikimedia.org/wikipedia/commons/8/83/MEGA_CHANNEL.png', color: '#222222', url: 'www.megatv.com' },
   'KATHIMERINI': { name: 'Kathimerini', logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNk0h-loKAwB5Ay_ohV5G8ItU88h7PY_RkPw&s', color: '#999999', url: 'www.kathimerini.gr' },
   'GOSSIP': { name: 'Gossip-tv', logo: 'https://cdn.gosmd.gr/img/1200/630/90/2016/05/19/1952016-14126.jpg?t=xRh4M6KRWfNTqee7gQzcuw', color: '#e6007e', url: 'www.gossip-tv.gr' },
+  'OPENTV': { name: 'Open TV', logo: 'https://upload.wikimedia.org/wikipedia/commons/5/55/OpenLogo.svg', color: '#fa8301', url: 'www.tvopen.gr' },
   'RUMOR': { name: 'Rumor / Gossip', color: '#fbbf24', url: null }
 };
 
